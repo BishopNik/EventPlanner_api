101ca118cecab32cce104e3c25831ed76c61f96e
Andrew <128685383+BishopNik@users.noreply.github.com>
2024-05-19 22:55:57 +0200

    fix fetch

--- controllers/getEvents.js
+++ controllers/getEvents.js
@@ -1,17 +1,19 @@
 /** @format */
 
 import axios from 'axios';
+import queryString from 'query-string';
 
 export const getEvents = async (req, res) => {
 	const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
 
 	const urlObj = new URL(fullUrl);
 	const urlParams = queryString.parse(urlObj.search);
+
 	try {
 		const response = await axios.get(
-			`https://kudago.com/public-api/v1.4/events?${req.originalUrl}`
+			`https://kudago.com/public-api/v1.4/events/?expand=${urlParams.expand}&fields=${urlParams.fields}&lang=${urlParams.lang}&location=${urlParams.location}&page=${urlParams.page}`
 		);
-		res.json(response);
+		res.json(response.data);
 	} catch (error) {
 		res.status(500).send('Error fetching data');
 	}
